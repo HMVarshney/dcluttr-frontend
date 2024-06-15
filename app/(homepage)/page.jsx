36df51387e332f9e0eb54a1fcea2a02ee2e442c0
Shiva Singh <shivasingh.200087@gmail.com>
2024-06-15 13:11:59 +0530

    WIP:YouCanTrustSection done

--- app/(homepage)/page.jsx
+++ app/(homepage)/page.jsx
@@ -1,10 +1,12 @@
 import FAQsSection from "./_components/FAQsSection";
+import YouCanTrustSection from "./_components/YouCanTrustSection";
 import HeroSection from "./_components/HeroSection";
 
 export default function Home() {
   return (
     <main className="h-full">
       <HeroSection />
+      <YouCanTrustSection />
       <FAQsSection />
     </main>
   );
