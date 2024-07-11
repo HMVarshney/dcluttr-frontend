6a05aafb881226bca54db2b6bf86061f9fe2ec00
Shiva Singh <shivasingh.200087@gmail.com>
2024-07-11 07:32:38 +0530

    WIP:bug fixed

--- app/(dashboards)/_components/OrgSidebar.jsx
+++ app/(dashboards)/_components/OrgSidebar.jsx
@@ -11,35 +11,35 @@ import { DashboardContext } from '../layout'
 export default function OrgSideBar() {
     const { sideBarClose: isOpen, setSideBarClose: setOpen } = useContext(DashboardContext)
     return (
-        <div className={cn('h-full min-w-[236px] w-[236px] transition-all', { 'min-w-[74px] w-[74px]': isOpen })}>
+        <div className={cn('h-full min-w-[236px] w-[236px] transition-all', { 'min-w-[60px] w-[60px]': isOpen })}>
             <Navbar isOpen={isOpen} />
             <div className='hidden lg:flex flex-col gap-y-6 w-full p-4 bg-[#F8F8F8] h-[calc(100%-70px)]'>
                 <div className='space-y-1 w-full'>
-                    <Button className="bg-primary rounded-full h-7 w-7 p-1" onClick={() => setOpen(!isOpen)}>
-                        <CaretDoubleRight size={16} weight="bold" className={cn('transition-all', isOpen ? 'rotate-180' : 'rotate-0')} />
+                    <Button className={cn('bg-primary rounded-full h-7 w-7 p-1 transition-all', isOpen ? 'rotate-180 ' : 'rotate-0 ')} onClick={() => setOpen(!isOpen)}>
+                        <CaretDoubleRight size={16} weight="bold" />
                     </Button>
-                    <Button asChild variant={"ghost"} size={"lg"} className='w-full font-normal justify-start px-2'>
+                    <Button asChild variant={"ghost"} size={"lg"} className={cn('w-full font-normal justify-start transition-all', isOpen ? 'px-1' : 'px-4')}>
                         <Link href={"/dashboard"}>
-                            <Home className='h-5 w-5 text-icon mr-2' />
-                            <span className={cn('text-sm font-medium transition-all max-w-96 opacity-100 line-clamp-1', { 'max-w-0 opacity-0': isOpen })}>Dashboard</span>
+                            <Home className='h-5 w-5 text-icon' />
+                            <span className={cn('text-sm font-medium transition-all max-w-96 opacity-100 line-clamp-1 ml-2', { 'max-w-0 opacity-0 ml-0': isOpen })}>Dashboard</span>
                         </Link>
                     </Button>
-                    <Button asChild variant={"ghost"} size={"lg"} className='w-full font-normal justify-start px-2'>
+                    <Button asChild variant={"ghost"} size={"lg"} className={cn('w-full font-normal justify-start transition-all', isOpen ? 'px-1' : 'px-4')}>
                         <Link href={"/"}>
-                            <TvMinimal className='h-5 w-5 text-icon mr-2' />
-                            <span className={cn('text-sm font-medium transition-all max-w-96 opacity-100 line-clamp-1', { 'max-w-0 opacity-0': isOpen })}>Performance</span>
+                            <TvMinimal className='h-5 w-5 text-icon' />
+                            <span className={cn('text-sm font-medium transition-all max-w-96 opacity-100 line-clamp-1 ml-2', { 'max-w-0 opacity-0 ml-0': isOpen })}>Performance</span>
                         </Link>
                     </Button>
-                    <Button asChild variant={"ghost"} size={"lg"} className='w-full font-normal justify-start px-2'>
+                    <Button asChild variant={"ghost"} size={"lg"} className={cn('w-full font-normal justify-start transition-all', isOpen ? 'px-1' : 'px-4')}>
                         <Link href={"/"}>
-                            <Box className='h-5 w-5 text-icon mr-2' />
-                            <span className={cn('text-sm font-medium transition-all max-w-96 opacity-100 line-clamp-1', { 'max-w-0 opacity-0': isOpen })}>Products</span>
+                            <Box className='h-5 w-5 text-icon' />
+                            <span className={cn('text-sm font-medium transition-all max-w-96 opacity-100 line-clamp-1 ml-2', { 'max-w-0 opacity-0 ml-0': isOpen })}>Products</span>
                         </Link>
                     </Button>
-                    <Button asChild variant={"ghost"} size={"lg"} className='w-full font-normal justify-start px-2'>
+                    <Button asChild variant={"ghost"} size={"lg"} className={cn('w-full font-normal justify-start transition-all', isOpen ? 'px-1' : 'px-4')}>
                         <Link href={"/"}>
-                            <ImagesIcon className='h-5 w-5 text-icon mr-2' />
-                            <span className={cn('text-sm font-medium transition-all max-w-96 opacity-100 line-clamp-1', { 'max-w-0 opacity-0': isOpen })}>Creatives</span>
+                            <ImagesIcon className='h-5 w-5 text-icon' />
+                            <span className={cn('text-sm font-medium transition-all max-w-96 opacity-100 line-clamp-1 ml-2', { 'max-w-0 opacity-0 ml-0': isOpen })}>Creatives</span>
                         </Link>
                     </Button>
                 </div>
--- app/(dashboards)/_components/Sidebar/Navbar.jsx
+++ app/(dashboards)/_components/Sidebar/Navbar.jsx
@@ -13,11 +13,11 @@ import { cn } from '@/lib/utils'
 
 export default function Navbar({ isOpen }) {
     return (
-        <div className='w-full p-4 border-b flex gap-4'>
+        <div className='w-full py-4 border-b flex gap-4'>
             <DropdownMenu>
                 <DropdownMenuTrigger asChild>
-                    <div className='h-9 border rounded-md w-full flex items-center gap-2 p-1.5 cursor-pointer' >
-                        <div className='text-[10px] bg-[#309E96] aspect-square min-w-6 content-center text-center rounded-md text-white'>
+                    <div className='h-9 border rounded-md w-full flex items-center p-1.5 cursor-pointer' >
+                        <div className='text-[10px] bg-[#309E96] aspect-square min-w-6 content-center text-center rounded-md text-white mr-2'>
                             SS
                         </div>
                         <div className={cn('text-sm font-semibold transition-all max-w-96 opacity-100 line-clamp-1', { 'max-w-0 opacity-0': isOpen })}>Start Struck by...</div>
