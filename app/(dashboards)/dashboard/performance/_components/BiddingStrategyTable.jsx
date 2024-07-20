7204f4db825783d56eae23f17380bd7aa1fbed57
Shiva Singh <shivasingh.200087@gmail.com>
2024-07-20 14:42:59 +0530

    BiddingStrategyTables done

--- app/(dashboards)/dashboard/performance/_components/BiddingStrategyTable.jsx
+++ app/(dashboards)/dashboard/performance/_components/BiddingStrategyTable.jsx
@@ -11,7 +11,7 @@ import {
     TableHeader,
     TableRow,
 } from "@/components/ui/table";
-import { memo, useEffect, useState } from "react";
+import { memo, useEffect, useMemo, useState } from "react";
 import {
     flexRender,
     getCoreRowModel,
@@ -22,89 +22,20 @@ import {
 import { Checkbox } from "@/components/ui/checkbox";
 import { useDispatch, useSelector } from "react-redux";
 import { getBiddingStrategy } from "@/lib/store/features/metaAdsSlice";
+import { Skeleton } from "@/components/ui/skeleton";
+import { cn } from "@/lib/utils";
 
-export const columns = [
-    {
-        accessorKey: "google_bidding_strategy.count",
-        header: ({ column }) => {
-            return (
-                <div className="flex items-center gap-3">
-                    <Checkbox className="rounded-lg bg-white z-10" />
-                    <div className="text-[#4E5E5A] font-medium text-sm">
-                        Strategy
-                    </div>
-                </div>
-            );
-        },
-        cell: ({ row }) => (
-            <div className="flex items-center gap-3">
-                <Checkbox className="rounded-lg bg-white z-10" />
-                <div className="text-[#4E5E5A] font-medium text-sm">{row.original?.["google_bidding_strategy.count"]}</div>
-            </div>
-        ),
-    },
-    {
-        accessorKey: "google_bidding_strategy.ad_spend_sum",
-        header: "Amount Spent",
-        cell: ({ row }) => (
-            <div className="text-[#4E5E5A] font-medium text-sm">{row.original?.["google_bidding_strategy.ad_spend_sum"]}</div>
-        ),
-    },
-    {
-        id: "google_bidding_strategy.roas",
-        header: "ROAS (Website)",
-        cell: ({ row }) => {
-            return (
-                <div className="text-[#4E5E5A] font-medium text-sm">{row.original?.["google_bidding_strategy.roas"]}</div>
-            );
-        },
-    },
-    {
-        id: "google_bidding_strategy.purchase_sum",
-        header: "Cost Per Website Purchase",
-        cell: ({ row }) => {
-            return (
-                <div className="text-[#4E5E5A] font-medium text-sm">{row.original?.["google_bidding_strategy.purchase_sum"]}</div>
-            );
-        },
-    },
-    {
-        id: "google_bidding_strategy.ctr",
-        header: "Links CTR",
-        cell: ({ row }) => {
-            return (
-                <div className="text-[#4E5E5A] font-medium text-sm">{row.original?.["google_bidding_strategy.ctr"]}</div>
-            );
-        },
-    }
-];
-
-function BiddingStrategyTable() {
+export default function BiddingStrategyTable() {
+    const isOpen = useSelector((state) => state.user.sideBarClose);
     const dispatch = useDispatch();
     const { loading, error, data } = useSelector((state) => state.metaAds.biddingStrategy);
+    console.log("Component rerendered", loading, error, data);
 
     useEffect(() => {
         dispatch(getBiddingStrategy());
     }, []);
-
-    // const [sorting, setSorting] = useState([]);
-
-    const table = useReactTable({
-        data: data?.results?.[0]?.data || [],
-        columns,
-        state: {
-            // sorting,
-        },
-        // onSortingChange: setSorting,
-        getCoreRowModel: getCoreRowModel(),
-        // getPaginationRowModel: getPaginationRowModel(),
-        // getSortedRowModel: getSortedRowModel(),
-    });
-
-    console.log("Component rerendered", loading, error, data);
-
     return (
-        <div className=" ">
+        <div className={cn(' w-[calc(100vw-332px)]', { 'w-[calc(100vw-174px)]': isOpen })}>
             <div className='flex items-center justify-center gap-2 p-6'>
                 <div className='mr-auto'>
                     <div className='text-xl font-bold'>
@@ -120,57 +51,97 @@ function BiddingStrategyTable() {
                     </Button>
                 </EditTableAttribution>
             </div>
-            <div className="px-6 pb-8 ">
-                <div className="rounded-md border shadow w-full overflow-x-auto relative">
-                    <Table className="rounded-md bg-white">
-                        <TableHeader>
-                            {table.getHeaderGroups().map((headerGroup) => (
-                                <TableRow key={headerGroup.id}>
-                                    {headerGroup.headers.map((header) => {
-                                        return (
-                                            <TableHead key={header.id}>
-                                                {header.isPlaceholder
-                                                    ? null
-                                                    : flexRender(
-                                                        header.column.columnDef.header,
-                                                        header.getContext()
-                                                    )}
-                                            </TableHead>
-                                        );
-                                    })}
-                                </TableRow>
-                            ))}
-                        </TableHeader>
-                        <TableBody>
-                            {table.getRowModel().rows?.length ? (
-                                table.getRowModel().rows.map((row) =>
-                                    <TableRow
-                                        key={row.id}
-                                        data-state={row.getIsSelected() && "selected"}
-                                    >
-                                        {row.getVisibleCells().map((cell) => (
-                                            <TableCell key={cell.id}>
-                                                {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                                            </TableCell>
-                                        ))}
-                                    </TableRow>
-                                )
-                            ) : (
-                                <TableRow>
-                                    <TableCell
-                                        colSpan={columns.length}
-                                        className="h-24 text-center"
-                                    >
-                                        No results.
-                                    </TableCell>
-                                </TableRow>
-                            )}
-                        </TableBody>
-                    </Table>
+            <div className="px-6 pb-8 w-full overflow-x-auto">
+                <div className="rounded-md border shadow max-w-full overflow-x-auto relative">
+                    {loading ?
+                        <Skeleton />
+                        : <BiddingStrategyTables annotation={data?.results?.[0]?.annotation} data={data?.results?.[0]?.data} />
+                    }
                 </div>
             </div>
         </div>
-    );
+    )
 }
 
-export default memo(BiddingStrategyTable);
+
+export function BiddingStrategyTables({ annotation, data }) {
+    const columns = useMemo(() => {
+        return Object.entries(annotation.measures).map(([key, value]) => ({
+            accessorKey: key?.replace("google_bidding_strategy.", ""),
+            header: value.shortTitle || "",
+        }));
+    }, [annotation]);
+
+    const transformedData = data.map(item => {
+        const flattenedData = {};
+        for (const [key, value] of Object.entries(item)) {
+            // Flatten the nested properties into a single level
+            flattenedData[key?.replace("google_bidding_strategy.", "")] = value;
+        }
+        return flattenedData;
+    });
+
+
+    // const [sorting, setSorting] = useState([]);
+
+    const table = useReactTable({
+        data: transformedData || [],
+        columns,
+        state: {
+            // sorting,
+        },
+        // onSortingChange: setSorting,
+        getCoreRowModel: getCoreRowModel(),
+        // getPaginationRowModel: getPaginationRowModel(),
+        // getSortedRowModel: getSortedRowModel(),
+    });
+
+
+    return (
+        <Table className="rounded-md bg-white">
+            <TableHeader>
+                {table.getHeaderGroups().map((headerGroup) => (
+                    <TableRow key={headerGroup.id}>
+                        {headerGroup.headers.map((header) => {
+                            return (
+                                <TableHead key={header.id}>
+                                    {header.isPlaceholder
+                                        ? null
+                                        : flexRender(
+                                            header.column.columnDef.header,
+                                            header.getContext()
+                                        )}
+                                </TableHead>
+                            );
+                        })}
+                    </TableRow>
+                ))}
+            </TableHeader>
+            <TableBody>
+                {table.getRowModel().rows?.length ? (
+                    table.getRowModel().rows.map((row) =>
+                        <TableRow
+                            key={row.id}
+                            data-state={row.getIsSelected() && "selected"}
+                        >
+                            {row.getVisibleCells().map((cell) => (
+                                <TableCell key={cell.id}>
+                                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                </TableCell>
+                            ))}
+                        </TableRow>
+                    )
+                ) : (
+                    <TableRow>
+                        <TableCell
+                            colSpan={columns.length}
+                            className="h-24 text-center"
+                        >
+                            No results.
+                        </TableCell>
+                    </TableRow>
+                )}
+            </TableBody>
+        </Table>
+    );
+}
