b81a9ceb808ea33739b9a399f8519fd99e742190
Shiva Singh <shivasingh.200087@gmail.com>
2024-07-26 16:47:49 +0530

    WIP:creative page

---
+++ app/(dashboards)/dashboard/creatives/_components/CreativesChart.jsx
@@ -0,0 +1,159 @@
+"use client"
+
+import React, { Fragment } from 'react';
+import { AreaChart as Charts, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import moment from 'moment';
+import { Skeleton } from '@/components/ui/skeleton';
+import CustomTooltip from '@/components/CustomTooltip';
+import CustomActiveDot from '@/components/CustomActiveDot';
+import Image from 'next/image';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+import { ArrowUp } from 'lucide-react';
+
+
+console.error = (...args) => {
+  if (/defaultProps/.test(args[0])) return;
+  console.log(...args);
+};
+
+export default function CreativesChart({ isLoading = false, data = {}, details, }) {
+  return (
+    <div className='border border-[#F1F1F1] shadow-[0px_1px_0px_0px_rgba(0,0,0,0.12)] rounded-lg bg-white w-full overflow-hidden group'>
+      <ChartHeader />
+      <div className='flex items-center justify-between px-4 pt-2.5'>
+        <div className=' text-2xl font-bold text-black'>
+          ₹2,45,982
+        </div>
+        <div className=''>
+          <div className='text-sm font-semibold text-green-600 flex items-center justify-end'>
+            <ArrowUp className='w-4' />
+            2.4%
+          </div>
+          <div className='text-[10px] text-gray-400 text-right'>
+            vs 2.69 last month
+          </div>
+        </div>
+      </div>
+      {isLoading
+        ? <Skeleton className="w-[calc(100%-32px)] h-[212px] my-4 rounded-md mx-auto" />
+        : <ResponsiveContainer width="100%" height={212}>
+          <Charts
+            data={data.data}
+            margin={{
+              top: 10, right: 16, left: -20, bottom: 0,
+            }}
+          >
+            <CartesianGrid
+              strokeWidth={1}
+              stroke="#D1D3DA8F"
+            />
+            <XAxis
+              dataKey={data.dataKeyXAxis
+                ? (ele) => moment(ele["orders.created_at"]).format('D MMM YY')
+                : null} axisLine={false} tickLine={false}
+              tick={{ fill: '#6B7583', fontSize: 10, fontWeight: 400 }} />
+
+            <YAxis axisLine={false} tickLine={false} tick={{ fill: '#6B7583', fontSize: 10, fontWeight: 400 }} />
+
+            <Tooltip content={<CustomTooltip />} />
+
+            {data.series?.map(ele => <Fragment key={ele.name}>
+              <Area
+                type="monotone"
+                dataKey={ele.dataKey}
+                stroke={ele.color}
+                strokeDasharray={ele.type !== 'area' ? "2 2" : null}
+                fillOpacity={1} fill={`url(#${ele.name}-${ele.id})`}
+                activeDot={<CustomActiveDot />}
+              />
+              {ele.type === 'area' &&
+                <defs>
+                  <linearGradient id={`${ele.name}-${ele.id}`} x1="0" y1="0" x2="0" y2="1">
+                    <stop offset="5%" stopColor={ele.color} stopOpacity={0.15} />
+                    <stop offset="95%" stopColor={ele.color} stopOpacity={0} />
+                  </linearGradient>
+                </defs>}
+            </Fragment>)}
+
+          </Charts>
+        </ResponsiveContainer>}
+      <ChartFooter data={data} />
+    </div>
+  );
+}
+
+
+
+export function ChartHeader() {
+  return (
+    <>
+      <div className='flex gap-2.5 items-center p-4 border-b border-[#F1F1F1]'>
+        <Select>
+          <SelectTrigger className="w-[140px] ">
+            <SelectValue placeholder="Group By 1" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="O-1">Group By 1</SelectItem>
+            <SelectItem value="O-2">Group By 2</SelectItem>
+            <SelectItem value="O-3">Group By 3</SelectItem>
+          </SelectContent>
+        </Select>
+        <Select>
+          <SelectTrigger className="w-[100px] ">
+            <SelectValue placeholder="CV 1" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="O-1">CV 1</SelectItem>
+            <SelectItem value="O-2">CV 2</SelectItem>
+            <SelectItem value="O-3">CV 3</SelectItem>
+          </SelectContent>
+        </Select>
+        <div className='flex-1'></div>
+        <Select>
+          <SelectTrigger className="w-[190px]">
+            <SelectValue placeholder="Choose metric" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="O-1">Choose metric</SelectItem>
+            <SelectItem value="O-2">Choose metric 2</SelectItem>
+            <SelectItem value="O-3">Choose metric 3</SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
+    </>
+  )
+}
+
+
+
+
+export function ChartFooter() {
+  return (
+    <div className='py-4 px-5 text-xs font-semibold text-[#515153] border-t border-[#F1F1F1] flex items-center justify-end gap-4'>
+      <div className='p-2.5 rounded-full border aspect-square'>
+        <Image
+          src="/icons/google.svg"
+          alt="logo" width={100} height={100}
+          className="w-5 object-contain" />
+      </div>
+      <div className='p-2.5 rounded-full border border-primary flex items-center aspect-square'>
+        <Image
+          src="/icons/meta.svg"
+          alt="logo" width={100} height={100}
+          className="w-5 object-contain" />
+      </div>
+      <div className='p-2.5 rounded-full border aspect-square'>
+        <Image
+          src="/icons/shopify.svg"
+          alt="logo" width={100} height={100}
+          className="w-5 object-contain" />
+      </div>
+    </div>
+  )
+}
--- app/(dashboards)/dashboard/creatives/page.jsx
+++ app/(dashboards)/dashboard/creatives/page.jsx
@@ -1,10 +1,29 @@
+"use client"
+
+import { ScrollArea } from '@/components/ui/scroll-area'
+import React, { useEffect } from 'react'
+import CreativesChart from './_components/CreativesChart'
+import Header from './_components/Header'
+import { useDispatch, useSelector } from 'react-redux';
+import { cn } from '@/lib/utils'
+import { fetchCreativeDetails } from '@/lib/store/features/creativeSlice'
 
 
+export default function Page() {
+    const dispatch = useDispatch();
+    const isOpen = useSelector((state) => state.user.sideBarClose);
+    const creativeDetails = useSelector((state) => state.creative.creativeDetails);
+    console.log(creativeDetails);
+    useEffect(() => {
+        dispatch(fetchCreativeDetails())
+    }, [])
 
-import React from 'react'
-
-export default function page() {
     return (
-        <div>products</div>
+        <ScrollArea className={cn('rounded-md bg-[#FAFAFA] h-full border w-[calc(100vw-332px)]', { 'w-[calc(100vw-174px)]': isOpen })}>
+            <Header />
+            <div className='m-6 '>
+                <CreativesChart data={creativeDetails?.results?.[0]} />
+            </div>
+        </ScrollArea>
     )
 }
