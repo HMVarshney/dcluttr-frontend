19df4c3276bfde25a143fa6daa7780a2a48824ea
HMVarshney <varshney.hm@gmail.com>
2024-08-03 02:19:32 +0530

    feat: Export init; 1. Export on google campaign table

--- app/(dashboards)/dashboard/performance/google-ads/page.jsx
+++ app/(dashboards)/dashboard/performance/google-ads/page.jsx
@@ -1,25 +1,21 @@
-
-
-
-import { ScrollArea } from '@/components/ui/scroll-area'
-import React from 'react'
-import Header from '../_components/Header';
-import CampaignTypeDonutChart from '../_components/CampaignTypeDonutChart';
-// import LiveOrdersTable from '../_components/LiveOrdersTable';
-import GoogleAdsDetails from '../_components/CampaignWiseTable/GoogleAdsDetails';
-import BiddingStrategyTable from '../_components/BiddingStrategyTable';
-import CampaignTypeTable from '../_components/CampaignTypeTable';
+import { ScrollArea } from "@/components/ui/scroll-area";
+import React from "react";
+import Header from "../_components/Header";
+import CampaignTypeDonutChart from "../_components/CampaignTypeDonutChart";
+import GoogleAdsDetails from "../_components/CampaignWiseTable/GoogleAdsDetails";
+import BiddingStrategyTable from "../_components/BiddingStrategyTable";
+import CampaignTypeTable from "../_components/CampaignTypeTable";
 
 export default function page() {
-    return (
-        <ScrollArea className='rounded-md bg-[#FAFAFA] h-full border'>
-            <Header />
-            <GoogleAdsDetails />
-            <BiddingStrategyTable isGoogle={true} />
-            <CampaignTypeTable isGoogle={true} />
-            <div className='m-6 flex gap-4 w-1/2'>
-                <CampaignTypeDonutChart />
-            </div>
-        </ScrollArea>
-    )
+  return (
+    <ScrollArea className="rounded-md bg-[#FAFAFA] h-full border">
+      <Header />
+      <GoogleAdsDetails />
+      <BiddingStrategyTable isGoogle={true} />
+      <CampaignTypeTable isGoogle={true} />
+      <div className="m-6 flex gap-4 w-1/2">
+        <CampaignTypeDonutChart />
+      </div>
+    </ScrollArea>
+  );
 }
