521ee58e4a6fc8c6057ca0344d7ecc31d273876b
HMVarshney <varshney.hm@gmail.com>
2024-08-06 00:22:39 +0530

    feat: Brand search enabled

--- app/(dashboards)/stores/_components/AllBrands.jsx
+++ app/(dashboards)/stores/_components/AllBrands.jsx
@@ -6,56 +6,53 @@ import { Gear } from "phosphor-react";
 import React from "react";
 
 function getActionButton(brand, orgId) {
-    if (brand.brandSettings?.isApproved) {
-        return (
-            <Link href={`/dashboard?brand=${brand.id}&orgId=${orgId}`}>
-                <Button variant="outline">Go to dashboard</Button>
-            </Link>
-        );
-    } else {
-        return (
-            <Link href={`/welcome?brand=${brand.id}&orgId=${orgId}&step=3`}>
-                <Button variant="outline">Pending approval</Button>
-            </Link>
-        );
-    }
+  if (brand.brandSettings?.isApproved) {
+    return (
+      <Link href={`/dashboard?brand=${brand.id}&orgId=${orgId}`}>
+        <Button variant="outline">Go to dashboard</Button>
+      </Link>
+    );
+  } else {
+    return (
+      <Link href={`/welcome?brand=${brand.id}&orgId=${orgId}&step=3`}>
+        <Button variant="outline">Pending approval</Button>
+      </Link>
+    );
+  }
 }
 
 export default function AllBrands({ brandList, isLoadingBrandsList, orgId }) {
-    return (
-        <>
-            <div className="flex flex-col items-stretch justify-center gap-6  px-6 mt-6">
-                {isLoadingBrandsList ? (
-                    <>
-                        <Skeleton className="w-full h-32" />
-                        <Skeleton className="w-full h-32" />
-                        <Skeleton className="w-full h-32" />
-                    </>
-                ) : (
-                    brandList.map((item) => (
-                        <div
-                            key={item.id}
-                            className="flex items-center justify-center gap-2 py-6 px-4 bg-white rounded-lg shadow"
-                        >
-                            <Image
-                                src={item.brandLogo || "/image_placeholder.svg"}
-                                width={400}
-                                height={400}
-                                className="rounded-lg border w-36 h-20 object-cover"
-                                alt={item.brandName}
-                            />
-                            <div className="mr-auto ml-4">
-                                <div className="text-base font-semibold">{item.brandName}</div>
-                                <div className="text-[#919191] text-sm underline">{item.brandWebsite}</div>
-                            </div>
-                            {getActionButton(item, orgId)}
-                            <Button variant="icon">
-                                <Gear size={24} />
-                            </Button>
-                        </div>
-                    ))
-                )}
+  return (
+    <>
+      <div className="flex flex-col items-stretch justify-center gap-6  px-6 mt-6">
+        {isLoadingBrandsList ? (
+          <>
+            <Skeleton className="w-full h-32" />
+            <Skeleton className="w-full h-32" />
+            <Skeleton className="w-full h-32" />
+          </>
+        ) : (
+          brandList.map((item) => (
+            <div key={item.id} className="flex items-center justify-center gap-2 py-6 px-4 bg-white rounded-lg shadow">
+              <Image
+                src={item.brandLogo || "/image_placeholder.svg"}
+                width={400}
+                height={400}
+                className="rounded-lg border w-36 h-20 object-cover"
+                alt={item.brandName}
+              />
+              <div className="mr-auto ml-4">
+                <div className="text-base font-semibold">{item.brandName}</div>
+                <div className="text-[#919191] text-sm underline">{item.brandWebsite}</div>
+              </div>
+              {getActionButton(item, orgId)}
+              <Button variant="icon">
+                <Gear size={24} />
+              </Button>
             </div>
-        </>
-    );
+          ))
+        )}
+      </div>
+    </>
+  );
 }
--- app/(dashboards)/stores/page.jsx
+++ app/(dashboards)/stores/page.jsx
@@ -11,54 +11,54 @@ import { useSelector } from "react-redux";
 import { Input } from "@/components/ui/input";
 
 function Page() {
-    const [name, setName] = useState("");
-    const { isLoadingBrandsList, brandsList } = useSelector((state) => state.brand);
-    const { organizationDetails, status } = useSelector((state) => state.organization);
+  const [searchedName, setSearchedName] = useState("");
+  const { isLoadingBrandsList, brandsList } = useSelector((state) => state.brand);
+  const { organizationDetails, status } = useSelector((state) => state.organization);
 
-    const isLoading = status === "loading" || isLoadingBrandsList;
-    const currentOrgBrands = useMemo(() => {
-        if (!organizationDetails) return [];
-        return brandsList.filter((brand) => brand.organizationId === organizationDetails.id);
-    }, [isLoadingBrandsList, organizationDetails]);
+  const isLoading = status === "loading" || isLoadingBrandsList;
+  const currentOrgBrands = useMemo(() => {
+    if (!organizationDetails) return [];
+    return brandsList.filter((brand) => brand.organizationId === organizationDetails.id);
+  }, [brandsList, organizationDetails]);
 
-    return (
-        <ScrollArea className="rounded-md bg-[#FAFAFA] h-full border">
-            <div className="sticky top-0 z-10">
-                <div className="flex items-center justify-center gap-2 py-3 px-6 bg-white border-b">
-                    <div className="mr-auto">
-                        <Breadcrumb>
-                            <BreadcrumbList>
-                                <BreadcrumbItem>
-                                    <BreadcrumbLink href="/stores">All stores</BreadcrumbLink>
-                                </BreadcrumbItem>
-                            </BreadcrumbList>
-                        </Breadcrumb>
-                        <div className="text-[#919191] text-xs">Last updated: 12:56 PM</div>
-                    </div>
+  const filteredBrands = useMemo(() => {
+    if (!searchedName) return currentOrgBrands;
+    return currentOrgBrands.filter((brand) => brand.brandName.toLowerCase().includes(searchedName.toLowerCase()));
+  }, [currentOrgBrands, searchedName]);
 
-                    <Input
-                        className="w-[200px]"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                        placeholder="🔍 Search for brand"
-                    />
-                    <Button variant="outline" className="px-2.5">
-                        <Plus className="w-4 h-4 mr-2" />
-                        <div className="font-medium text-sm">Add a new store</div>
-                    </Button>
-                </div>
-            </div>
-            {currentOrgBrands?.length > 0 || isLoading ? (
-                <AllBrands
-                    brandList={currentOrgBrands}
-                    orgId={organizationDetails.id}
-                    isLoadingBrandsList={isLoading}
-                />
-            ) : (
-                <EmptyStores />
-            )}
-        </ScrollArea>
-    );
+  return (
+    <ScrollArea className="rounded-md bg-[#FAFAFA] h-full border">
+      <div className="sticky top-0 z-10">
+        <div className="flex items-center justify-center gap-2 py-3 px-6 bg-white border-b">
+          <div className="mr-auto">
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem>
+                  <BreadcrumbLink href="/stores">All stores</BreadcrumbLink>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+            <div className="text-[#919191] text-xs">Last updated: 12:56 PM</div>
+          </div>
+          <Input
+            className="w-[200px]"
+            value={searchedName}
+            onChange={(e) => setSearchedName(e.target.value)}
+            placeholder="🔍 Search for brand"
+          />
+          <Button variant="outline" className="px-2.5">
+            <Plus className="w-4 h-4 mr-2" />
+            <div className="font-medium text-sm">Add a new store</div>
+          </Button>
+        </div>
+      </div>
+      {currentOrgBrands?.length > 0 || isLoading ? (
+        <AllBrands brandList={filteredBrands} orgId={organizationDetails.id} isLoadingBrandsList={isLoading} />
+      ) : (
+        <EmptyStores />
+      )}
+    </ScrollArea>
+  );
 }
 
 export default Page;
