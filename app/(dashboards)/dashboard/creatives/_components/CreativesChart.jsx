2088722b6588a0c770e89e7e9b67afbe9e1b1c45
Shiva Singh <shivasingh.200087@gmail.com>
2024-08-24 20:10:21 +0530

    WIP:creatives ui bug fixed

--- app/(dashboards)/dashboard/creatives/_components/CreativesChart.jsx
+++ app/(dashboards)/dashboard/creatives/_components/CreativesChart.jsx
@@ -1,106 +1,99 @@
-"use client"
+"use client";
 
-import React, { Fragment } from 'react';
-import { AreaChart, Line, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
-import moment from 'moment';
-import { Skeleton } from '@/components/ui/skeleton';
-import CustomTooltip from '@/components/CustomTooltip';
-import CustomActiveDot from '@/components/CustomActiveDot';
-import Image from 'next/image';
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
-import { ArrowUp } from 'lucide-react';
-
+import React, { Fragment } from "react";
+import { AreaChart, Line, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
+import moment from "moment";
+import { Skeleton } from "@/components/ui/skeleton";
+import CustomTooltip from "@/components/CustomTooltip";
+import CustomActiveDot from "@/components/CustomActiveDot";
+import Image from "next/image";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { ArrowUp } from "lucide-react";
+import { useSelector } from "react-redux";
+import { cn } from "@/lib/utils";
 
 console.error = (...args) => {
   if (/defaultProps/.test(args[0])) return;
   console.log(...args);
 };
 
-export default function CreativesChart({ data, isLoading = false, results = {}, }) {
+export default function CreativesChart({ data, isLoading = false, results = {} }) {
   // const annotation = results?.annotation
   // const data = results?.data
+  const showMainChart = useSelector((state) => state.user.showMainChart);
 
   return (
-    <div className='m-6 border border-[#F1F1F1] shadow-[0px_1px_0px_0px_rgba(0,0,0,0.12)] rounded-lg bg-white overflow-hidden group'>
+    <div
+      className={cn(
+        "m-6 border border-[#F1F1F1] shadow-[0px_1px_0px_0px_rgba(0,0,0,0.12)] rounded-lg bg-white overflow-hidden group max-h-[600px] opacity-100 transition-all",
+        { " border-none shadow-none max-h-0 m-0 opacity-0": !showMainChart }
+      )}
+    >
       <ChartHeader />
-      <div className='flex items-center justify-between px-4 pt-2.5'>
-        <div className=' text-2xl font-bold text-black'>
-          ₹2,45,982
-        </div>
-        <div className=''>
-          <div className='text-sm font-semibold text-green-600 flex items-center justify-end'>
-            <ArrowUp className='w-4' />
+      <div className="flex items-center justify-between px-4 pt-2.5">
+        <div className=" text-2xl font-bold text-black">₹2,45,982</div>
+        <div className="">
+          <div className="text-sm font-semibold text-green-600 flex items-center justify-end">
+            <ArrowUp className="w-4" />
             2.4%
           </div>
-          <div className='text-[10px] text-gray-400 text-right'>
-            vs 2.69 last month
-          </div>
+          <div className="text-[10px] text-gray-400 text-right">vs 2.69</div>
         </div>
       </div>
-      {isLoading
-        ? <Skeleton className="w-[calc(100%-32px)] h-[212px] my-4 rounded-md mx-auto" />
-        : <ResponsiveContainer width="100%" height={212}>
+      {isLoading ? (
+        <Skeleton className="w-[calc(100%-32px)] h-[212px] my-4 rounded-md mx-auto" />
+      ) : (
+        <ResponsiveContainer width="100%" height={160}>
           <AreaChart
-            data={data}
+            data={data.data}
             margin={{
-              top: 10, right: 16, left: -20, bottom: 0,
+              top: 10,
+              right: 16,
+              left: -20,
+              bottom: 0
             }}
           >
-            <CartesianGrid
-              strokeWidth={1}
-              stroke="#D1D3DA8F"
-            />
-            <XAxis
-              dataKey={"date"} axisLine={false} tickLine={false}
-              tick={{ fill: '#6B7583', fontSize: 10, fontWeight: 400 }} />
+            <CartesianGrid strokeWidth={1} stroke="#d1d3da33" />
+            <XAxis dataKey={"x"} axisLine={false} tickLine={false} tick={{ fill: "#6B7583", fontSize: 10, fontWeight: 400 }} />
 
-            <YAxis axisLine={false} tickLine={false} tick={{ fill: '#6B7583', fontSize: 10, fontWeight: 400 }} />
+            <YAxis axisLine={false} tickLine={false} tick={{ fill: "#6B7583", fontSize: 10, fontWeight: 400 }} />
 
             <Tooltip content={<CustomTooltip />} />
 
-            {Object.entries(data?.[0] ?? {})
-              ?.map(([k, v], i) => {
-                console.log(k, v);
-                if (["purchase_value_sum", "count", "link_clicks_sum", ""].includes(k)) {
-                  return (
-                    <Fragment key={k}>
-                      <Area
-                        type="monotone"
-                        dataKey={k}
-                        // stroke={ele.color}
-                        strokeDasharray={null}//"2 2"}
-                        fillOpacity={1} fill={`url(#${k})`}
-                        activeDot={<CustomActiveDot />}
-                      />
-                      <defs>
-                        <linearGradient id={`${k}`} x1="0" y1="0" x2="0" y2="1">
-                          <stop offset="5%" stroke="#2EB76F" stopOpacity={0.15} />
-                          <stop offset="95%" stroke="#2EB76F" stopOpacity={0} />
-                        </linearGradient>
-                      </defs>
-                    </Fragment>
-                  )
-                }
-              })}
+            {data.series?.map((ele) => (
+              <Fragment key={ele.name}>
+                <Area
+                  type="monotone"
+                  dataKey={ele.dataKey}
+                  stroke={ele.color}
+                  strokeWidth={1.2}
+                  strokeDasharray={ele.type !== "area" ? "4 2" : null}
+                  fillOpacity={1}
+                  fill={`url(#${ele.name}-${ele.id})`}
+                  activeDot={<CustomActiveDot />}
+                />
+                {ele.type === "area" && (
+                  <defs>
+                    <linearGradient id={`${ele.name}-${ele.id}`} x1="0" y1="0" x2="0" y2="1">
+                      <stop offset="5%" stopColor={ele.color} stopOpacity={0.15} />
+                      <stop offset="95%" stopColor={ele.color} stopOpacity={0} />
+                    </linearGradient>
+                  </defs>
+                )}
+              </Fragment>
+            ))}
           </AreaChart>
-        </ResponsiveContainer>}
+        </ResponsiveContainer>
+      )}
       <ChartFooter data={data} />
     </div>
   );
 }
 
-
-
 export function ChartHeader() {
   return (
     <>
-      <div className='flex gap-2.5 items-center p-4 border-b border-[#F1F1F1]'>
+      <div className="flex gap-2.5 items-center p-4 border-b border-[#F1F1F1]">
         <Select>
           <SelectTrigger className="w-[140px] ">
             <SelectValue placeholder="Group By 1" />
@@ -121,7 +114,7 @@ export function ChartHeader() {
             <SelectItem value="O-3">CV 3</SelectItem>
           </SelectContent>
         </Select>
-        <div className='flex-1'></div>
+        <div className="flex-1"></div>
         <Select>
           <SelectTrigger className="w-[190px]">
             <SelectValue placeholder="Choose metric" />
@@ -134,33 +127,21 @@ export function ChartHeader() {
         </Select>
       </div>
     </>
-  )
+  );
 }
-
-
-
 
 export function ChartFooter() {
   return (
-    <div className='py-4 px-5 text-xs font-semibold text-[#515153] border-t border-[#F1F1F1] flex items-center justify-end gap-4'>
-      <div className='p-2.5 rounded-full border aspect-square'>
-        <Image
-          src="/icons/google.svg"
-          alt="logo" width={100} height={100}
-          className="w-5 object-contain" />
+    <div className="py-4 px-5 text-xs font-semibold text-[#515153] border-t border-[#F1F1F1] flex items-center justify-end gap-4">
+      <div className="p-2.5 rounded-full border aspect-square">
+        <Image src="/icons/google.svg" alt="logo" width={100} height={100} className="w-5 object-contain" />
       </div>
-      <div className='p-2.5 rounded-full border border-primary flex items-center aspect-square'>
-        <Image
-          src="/icons/meta.svg"
-          alt="logo" width={100} height={100}
-          className="w-5 object-contain" />
+      <div className="p-2.5 rounded-full border border-primary flex items-center aspect-square">
+        <Image src="/icons/meta.svg" alt="logo" width={100} height={100} className="w-5 object-contain" />
       </div>
-      <div className='p-2.5 rounded-full border aspect-square'>
-        <Image
-          src="/icons/shopify.svg"
-          alt="logo" width={100} height={100}
-          className="w-5 object-contain" />
+      <div className="p-2.5 rounded-full border aspect-square">
+        <Image src="/icons/shopify.svg" alt="logo" width={100} height={100} className="w-5 object-contain" />
       </div>
     </div>
-  )
+  );
 }
--- app/(dashboards)/dashboard/creatives/page.jsx
+++ app/(dashboards)/dashboard/creatives/page.jsx
@@ -1,31 +1,199 @@
-"use client"
+"use client";
 
-import { ScrollArea } from '@/components/ui/scroll-area'
-import React, { useEffect } from 'react'
-import CreativesChart from './_components/CreativesChart'
-import Header from './_components/Header'
-import { useDispatch, useSelector } from 'react-redux';
-import { cn } from '@/lib/utils'
-import { fetchCreativeDetails } from '@/lib/store/features/creativeSlice'
-import CreativesTable from './_components/CreativesTable'
+import { ScrollArea } from "@/components/ui/scroll-area";
+import React, { useEffect } from "react";
+import CreativesChart from "./_components/CreativesChart";
+import Header from "./_components/Header";
+import { useDispatch, useSelector } from "react-redux";
+import { cn } from "@/lib/utils";
+import { fetchCreativeDetails } from "@/lib/store/features/creativeSlice";
+import CreativesTable from "./_components/CreativesTable";
 
+const data = {
+  title: "Spends",
+  series: [
+    {
+      name: "Spends",
+      dataKey: "s",
+      id: 1,
+      color: "#9A66ED",
+      type: "area"
+    },
+    {
+      name: "Revenue",
+      dataKey: "r",
+      id: 2,
+      color: "#2EB76F",
+      type: "area"
+    }
+  ],
+  data: [
+    {
+      name: "A",
+      s: 8,
+      r: 1,
+      amt: 24,
+      x: "jan"
+    },
+    {
+      name: "B",
+      s: 5,
+      r: 10,
+      amt: 22,
+      x: "feb"
+    },
+    {
+      name: "C",
+      s: 12,
+      r: 10,
+      amt: 22,
+      x: "mar"
+    },
+    {
+      name: "D",
+      s: 10,
+      r: 17,
+      amt: 20,
+      x: "apr"
+    },
+    {
+      name: "E",
+      s: 14,
+      r: 25,
+      amt: 21,
+      x: "may"
+    },
+    {
+      name: "F",
+      s: 10,
+      r: 19,
+      amt: 25,
+      x: "jun"
+    },
+    {
+      name: "G",
+      s: 20,
+      r: 25,
+      amt: 21,
+      x: "jul"
+    }
+  ]
+};
+
+const data1 = {
+  title: "Spends",
+  series: [
+    {
+      name: "Spends",
+      dataKey: "s",
+      id: 3,
+      color: "#9A66ED",
+      type: "line"
+    },
+    {
+      name: "Revenue",
+      dataKey: "r",
+      id: 4,
+      color: "#2EB76F",
+      type: "area"
+    }
+  ],
+  data: [
+    {
+      name: "A",
+      s: 8,
+      r: 1,
+      amt: 24,
+      x: "jan"
+    },
+    {
+      name: "B",
+      s: 5,
+      r: 10,
+      amt: 22,
+      x: "feb"
+    },
+    {
+      name: "C",
+      s: 12,
+      r: 10,
+      amt: 22,
+      x: "mar"
+    },
+    {
+      name: "D",
+      s: 10,
+      r: 17,
+      amt: 20,
+      x: "apr"
+    },
+    {
+      name: "E",
+      s: 14,
+      r: 25,
+      amt: 21,
+      x: "may"
+    },
+    {
+      name: "F",
+      s: 10,
+      r: 19,
+      amt: 25,
+      x: "jun"
+    },
+    {
+      name: "G",
+      s: 20,
+      r: 25,
+      amt: 21,
+      x: "jul"
+    }
+  ]
+};
 
 export default function Page() {
-    const dispatch = useDispatch();
-    const isOpen = useSelector((state) => state.user.sideBarClose);
-    const { creativeDetails, isLoading } = useSelector((state) => state.creative);
-    console.log(creativeDetails);
-    useEffect(() => {
-        dispatch(fetchCreativeDetails())
-    }, [])
+  const dispatch = useDispatch();
+  const isOpen = useSelector((state) => state.user.sideBarClose);
+  const { creativeDetails, isLoading } = useSelector((state) => state.creative);
+  console.log(creativeDetails);
+  useEffect(() => {
+    dispatch(fetchCreativeDetails());
+  }, []);
 
-    return (
-        <ScrollArea className={cn('rounded-md bg-[#FAFAFA] h-full border w-[calc(100vw-332px)]', { 'w-[calc(100vw-174px)]': isOpen })}>
-            <Header />
-            <CreativesChart data={creativeDetails?.results?.[0]?.data} isLoading={isLoading} />
+  return (
+    <ScrollArea
+      className={cn("rounded-md bg-[#FAFAFA] h-full border w-[calc(100vw-332px)]", { "w-[calc(100vw-174px)]": isOpen })}
+    >
+      <Header />
+      <CreativesChart
+        data={{
+          ...data1,
+          data: [...data1.data, ...data1.data, ...data1.data],
+          series: [
+            {
+              name: "Total",
+              dataKey: "s",
+              id: 1,
+              color: "#E87C67",
+              type: "line"
+            },
+            {
+              name: "Sales",
+              dataKey: "r",
+              id: 2,
+              color: "#B1BA88",
+              type: "area"
+            }
+          ]
+        }}
+        isLoading={isLoading}
+      />
 
-            <CreativesTable data={creativeDetails?.results?.[0]?.data}
-                annotation={creativeDetails?.results?.[0]?.annotation} isLoading={isLoading} />
-        </ScrollArea>
-    )
+      <CreativesTable
+        data={creativeDetails?.results?.[0]?.data}
+        annotation={creativeDetails?.results?.[0]?.annotation}
+        isLoading={isLoading}
+      />
+    </ScrollArea>
+  );
 }
