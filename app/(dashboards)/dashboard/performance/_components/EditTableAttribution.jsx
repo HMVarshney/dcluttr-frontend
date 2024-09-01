f102c5011c0410896e1a2c458ae36551948014a6
HMVarshney <varshney.hm@gmail.com>
2024-09-02 02:31:41 +0530

    feat: Google ads performance page renderd on /board

--- app/(dashboards)/dashboard/performance/_components/EditTableAttribution.jsx
+++ app/(dashboards)/dashboard/performance/_components/EditTableAttribution.jsx
@@ -1,48 +1,93 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
-import { X } from "lucide-react";
-import { DotsSixVertical } from "phosphor-react";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import React, { useState } from "react";
 import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { X } from "lucide-react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { DotsSixVertical } from "phosphor-react";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 
-export default function EditTableAttribution({
-  children,
-  columns,
-  columnVisibility,
-  columnOrder,
-  setColumnVisibility,
-  setColumnOrdering
-}) {
+const defaultColumns = [
+  {
+    header: "Campaign Type",
+    accessorKey: "campaign_type",
+    cell: (info) => <div className="min-w-32">{info.getValue()}</div>
+  },
+  {
+    header: "Purchase Value Sum",
+    accessorKey: "purchase_value_sum",
+    cell: (info) => <div className="min-w-32">{info.getValue()}</div>
+  },
+  {
+    header: "Ad Spend Sum",
+    accessorKey: "ad_spend_sum",
+    cell: (info) => <div className="min-w-32">{info.getValue()}</div>
+  },
+  {
+    header: "Purchase Sum",
+    accessorKey: "purchase_sum",
+    cell: (info) => <div className="min-w-32">{info.getValue()}</div>
+  },
+  {
+    header: "Impressions Sum",
+    accessorKey: "impressions_sum",
+    cell: (info) => <div className="min-w-32">{info.getValue()}</div>
+  },
+  {
+    header: "Clicks Sum",
+    accessorKey: "clicks_sum",
+    cell: (info) => <div className="min-w-32">{info.getValue()}</div>
+  },
+  {
+    header: "Vtc Sum",
+    accessorKey: "vtc_sum",
+    cell: (info) => <div className="min-w-32">{info.getValue()}</div>
+  },
+  {
+    header: "Ctr",
+    accessorKey: "ctr",
+    cell: (info) => <div className="min-w-32">{info.getValue()}</div>
+  },
+  {
+    header: "Cpc",
+    accessorKey: "cpc",
+    cell: (info) => <div className="min-w-32">{info.getValue()}</div>
+  },
+  {
+    header: "Cpm",
+    accessorKey: "cpm",
+    cell: (info) => <div className="min-w-32">{info.getValue()}</div>
+  },
+  {
+    header: "Roas",
+    accessorKey: "roas",
+    cell: (info) => <div className="min-w-32">{info.getValue()}</div>
+  },
+  {
+    header: "Aov",
+    accessorKey: "aov",
+    cell: (info) => <div className="min-w-32">{info.getValue()}</div>
+  },
+  {
+    header: "Cpa",
+    accessorKey: "cpa",
+    cell: (info) => <div className="min-w-32">{info.getValue()}</div>
+  }
+];
+
+export default function EditTableAttribution({ children, columns }) {
   const [isOpen, setOpen] = useState(false);
-  const [titles, setTitles] = useState([]);
+  const [titles, setTitles] = useState((columns ?? defaultColumns)?.map((i) => i.header));
 
   const handleOnDragEnd = (result) => {
     if (!result.destination) return;
-
-    const items = [...columnOrder];
+    const items = titles;
     const [reorderedItem] = items.splice(result.source.index, 1);
     items.splice(result.destination.index, 0, reorderedItem);
-    console.log("dragend items", items);
-    setColumnOrdering(items);
+
+    setTitles(items);
   };
-
-  useEffect(() => {
-    const columnOrdering = columnOrder.reduce((prev, col) => {
-      const rawCol = columns.find((c) => c.key === col);
-      if (rawCol) {
-        prev.push({
-          id: rawCol.key,
-          title: rawCol.shortTitle
-        });
-      }
-      return prev;
-    }, []);
-    setTitles(columnOrdering);
-  }, [columnOrder, columns]);
-
   return (
     <Popover open={isOpen} onOpenChange={(e) => setOpen(e)}>
       <PopoverTrigger asChild>{children}</PopoverTrigger>
@@ -51,14 +96,13 @@ export default function EditTableAttribution({
           <div className=" text-base font-bold">Edit: Dcluttr Preset</div>
           <X className="w-5 h-5" onClick={() => setOpen(false)} />
         </div>
-
         <div className="py-3 px-6 text-sm font-medium text-[#031B1599]">INCLUDED ITEMS</div>
 
         <DragDropContext onDragEnd={handleOnDragEnd}>
           <Droppable droppableId="imageUrls" direction="vertical">
             {(provided) => (
               <div className="flex flex-col" {...provided.droppableProps} ref={provided.innerRef}>
-                {titles.map((t, i) => (
+                {titles.map((title, i) => (
                   <Draggable key={i} draggableId={`image-${i}`} index={i}>
                     {(provided, snapshot) => (
                       <div
@@ -71,14 +115,10 @@ export default function EditTableAttribution({
                         style={{ ...provided.draggableProps.style, opacity: snapshot.isDragging ? 0.8 : 1 }}
                       >
                         <DotsSixVertical size={16} color="#031B15" className="mr-4" />
-                        <div className="text-base font-semibold"> {t.title}</div>
+                        <div className="text-base font-semibold"> {title}</div>
                         <input
                           type="checkbox"
                           className={cn("min-w-4 h-4 accent-primary hover:accent-primary/80 rounded ml-auto", " cursor-pointer")}
-                          checked={!!columnVisibility[t.id]}
-                          onChange={(event) => {
-                            setColumnVisibility(t.id, event.target.checked);
-                          }}
                         />
                       </div>
                     )}
@@ -89,7 +129,8 @@ export default function EditTableAttribution({
             )}
           </Droppable>
           <div className="w-full flex items-center justify-end gap-2.5 p-4 border-t sticky bottom-0 bg-white">
-            <Button onClick={() => setOpen(false)}>Done</Button>
+            <Button variant="outline">Cancel</Button>
+            <Button variant="">Save</Button>
           </div>
         </DragDropContext>
       </PopoverContent>
--- app/board/dashboards.js
+++ app/board/dashboards.js
@@ -52,9 +52,9 @@ export const dashboardJSON = {
         },
         {
           visualizationType: "type1",
-          title: "Campaign Type",
+          title: "Meta Purchase Value Sum",
           id: "102",
-          logo: "/icons/google.svg",
+          logo: "/icons/meta.svg",
           description: "Find all the analytics for store",
           gridStackProperties: {
             x: 4,
@@ -75,7 +75,7 @@ export const dashboardJSON = {
           visualizationType: "type1",
           title: "Campaign Type",
           id: "102",
-          logo: "/icons/google.svg",
+          logo: "/icons/meta.svg",
           description: "Find all the analytics for store",
           gridStackProperties: {
             x: 8,
@@ -94,9 +94,9 @@ export const dashboardJSON = {
         },
         {
           visualizationType: "type1",
-          title: "Campaign Type",
+          title: "Shopify Total Orders Sum",
           id: "102",
-          logo: "/icons/google.svg",
+          logo: "/icons/shopify.svg",
           description: "Find all the analytics for store",
           gridStackProperties: {
             x: 0,
@@ -108,9 +108,9 @@ export const dashboardJSON = {
             noMove: true
           },
           query:
-            '[{"measures":["hybrid_performance_stream.meta_purchase_value_sum"],"timeDimensions":[{"dimension":"hybrid_performance_stream.created_at","compareDateRange":"${compare_date_range_query}"}]},{"measures":["hybrid_performance_stream.meta_purchase_value_sum"],"timeDimensions":[{"dimension":"hybrid_performance_stream.created_at","granularity":"${time_dimension_granularity}","compareDateRange":"${compare_date_range_query}"}],"order":[["hybrid_performance_stream.created_at","asc"]]}]',
-          columnOrder: ["date", "sales"],
-          selectedMetric: ["sales"],
+            '[{"measures":["hybrid_performance_stream.shopify_total_orders_sum"],"timeDimensions":[{"dimension":"hybrid_performance_stream.created_at","compareDateRange":"${compare_date_range_query}"}],"order":{"hybrid_performance_stream.created_at":"asc"}}, {"measures":["hybrid_performance_stream.shopify_total_orders_sum"],"timeDimensions":[{"dimension":"hybrid_performance_stream.created_at","granularity":"${time_dimension_granularity}","compareDateRange":"${compare_date_range_query}"}],"order":{"hybrid_performance_stream.created_at":"asc"}}]',
+          columnOrder: [],
+          selectedMetric: [],
           active: true
         },
         {
@@ -154,70 +154,7 @@ export const dashboardJSON = {
           columnOrder: ["date", "sales"],
           selectedMetric: ["sales"],
           active: true
-        },
-        {
-          visualizationType: "type1",
-          title: "Campaign Type",
-          id: "102",
-          logo: "/icons/google.svg",
-          description: "Find all the analytics for store",
-          gridStackProperties: {
-            x: 0,
-            y: 6,
-            w: 4,
-            h: 2,
-            locked: true,
-            noResize: true,
-            noMove: true
-          },
-          query:
-            '[{"measures":["hybrid_performance_stream.meta_purchase_value_sum"],"timeDimensions":[{"dimension":"hybrid_performance_stream.created_at","compareDateRange":"${compare_date_range_query}"}]},{"measures":["hybrid_performance_stream.meta_purchase_value_sum"],"timeDimensions":[{"dimension":"hybrid_performance_stream.created_at","granularity":"${time_dimension_granularity}","compareDateRange":"${compare_date_range_query}"}],"order":[["hybrid_performance_stream.created_at","asc"]]}]',
-          columnOrder: ["date", "sales"],
-          selectedMetric: ["sales"],
-          active: true
-        },
-        {
-          visualizationType: "gauge",
-          title: "Campaign Type",
-          id: "102",
-          logo: "/icons/google.svg",
-          description: "Find all the analytics for store",
-          gridStackProperties: {
-            x: 4,
-            y: 6,
-            w: 4,
-            h: 1,
-            locked: true,
-            noResize: true,
-            noMove: true
-          },
-          query:
-            '{"measures":["hybrid_performance_stream.google_ad_spend_sum"],"timeDimensions":[{"dimension":"hybrid_performance_stream.created_at","granularity":"week"}],"order":[["hybrid_performance_stream.created_at","asc"]]}',
-          columnOrder: ["date", "sales"],
-          selectedMetric: ["sales"],
-          active: false
         }
-        // {
-        //   visualizationType: "piechart",
-        //   title: "Campaign Type",
-        //   id: "102",
-        //   logo: "http://example.com/logo.png",
-        //   description: "Find all the analytics for store",
-        //   gridStackProperties: {
-        //     x: 0,
-        //     y: 12,
-        //     w: 4,
-        //     h: 2,
-        //     locked: true,
-        //     noResize: true,
-        //     noMove: true
-        //   },
-        //   query:
-        //     '{"measures":["google_campaign_type.count","google_campaign_type.purchase_value_sum","google_campaign_type.ad_spend_sum","google_campaign_type.purchase_sum","google_campaign_type.impressions_sum","google_campaign_type.clicks_sum","google_campaign_type.vtc_sum","google_campaign_type.ctr","google_campaign_type.cpc","google_campaign_type.cpm","google_campaign_type.roas","google_campaign_type.aov","google_campaign_type.cpa"],"dimensions":["google_campaign_type.name"],"timeDimensions":[],"order":{"google_campaign_type.count":"desc"}}',
-        //   columnOrder: ["date", "sales"],
-        //   selectedMetric: ["sales"],
-        //   active: false
-        // }
       ]
     },
     {
@@ -321,6 +258,63 @@ export const dashboardTableJSON = {
             "google_campaign_stream.aov",
             "google_campaign_stream.cpa"
           ],
+          selectedMetric: [],
+          active: true
+        },
+        {
+          visualizationType: "table",
+          title: "Bidding Strategy",
+          id: "102",
+          logo: "/icons/google.svg",
+          description: "Find all the analytics for store",
+          gridStackProperties: {
+            x: 0,
+            y: 4,
+            w: 12,
+            h: 2,
+            locked: true,
+            noResize: true,
+            noMove: true
+          },
+          query:
+            '{"measures":["google_bidding_strategy.count","google_bidding_strategy.purchase_value_sum","google_bidding_strategy.ad_spend_sum","google_bidding_strategy.purchase_sum","google_bidding_strategy.impressions_sum","google_bidding_strategy.clicks_sum","google_bidding_strategy.vtc_sum","google_bidding_strategy.ctr","google_bidding_strategy.cpc","google_bidding_strategy.cpm","google_bidding_strategy.roas","google_bidding_strategy.aov","google_bidding_strategy.cpa"],"timeDimensions":[{"dimension":"google_bidding_strategy.date","dateRange":["${time_dimension_date_range_from}","${time_dimension_date_range_to}"],"granularity":"${time_dimension_granularity}"}],"order":{"google_bidding_strategy.count":"desc"},"dimensions":["google_bidding_strategy.bidding_strategy"]}',
+          columnOrder: [
+            "google_bidding_strategy.count",
+            "google_bidding_strategy.bidding_strategy",
+            "google_bidding_strategy.purchase_value_sum",
+            "google_bidding_strategy.ad_spend_sum",
+            "google_bidding_strategy.purchase_sum",
+            "google_bidding_strategy.impressions_sum",
+            "google_bidding_strategy.clicks_sum",
+            "google_bidding_strategy.vtc_sum",
+            "google_bidding_strategy.ctr",
+            "google_bidding_strategy.cpc",
+            "google_bidding_strategy.cpm",
+            "google_bidding_strategy.roas",
+            "google_bidding_strategy.aov",
+            "google_bidding_strategy.cpa"
+          ],
+          selectedMetric: [],
+          active: true
+        },
+        {
+          visualizationType: "piechart",
+          title: "Campaign Type",
+          id: "102",
+          logo: "/icons/google.svg",
+          description: "Find all the analytics for store",
+          gridStackProperties: {
+            x: 0,
+            y: 8,
+            w: 4,
+            h: 1,
+            locked: true,
+            noResize: true,
+            noMove: true
+          },
+          query:
+            '[null,{"measures":["google_campaign_type.clicks_sum"],"timeDimensions":[{"dimension":"google_campaign_type.date","dateRange":["${time_dimension_date_range_from}","${time_dimension_date_range_to}"]}],"dimensions":["google_campaign_type.campaign_type"]}]',
+          columnOrder: [],
           selectedMetric: [],
           active: true
         }
